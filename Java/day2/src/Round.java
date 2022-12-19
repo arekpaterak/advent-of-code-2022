1f3f99d44764139edad5897fd152a456bb8a7762
AsgardianVoyager <86044128+AsgardianVoyager@users.noreply.github.com>
2022-12-19 20:36:38 +0100

    Update Round.java

--- Java/day2/src/Round.java
+++ Java/day2/src/Round.java
@@ -6,10 +6,13 @@ public class Round {
     String selectedShape;
     String outcome;
 
-    public Round(String opponentShape, String selectedShape) {
+    public Round(String opponentShape, String outcome) {
         this.opponentShape = shapesForSymbols.get(opponentShape);
-        this.selectedShape = shapesForSymbols.get(selectedShape);
-        determineOutcome();
+//        this.selectedShape = shapesForSymbols.get(selectedShape);
+//        determineOutcome();
+
+        this.outcome = outcomeForSymbols.get(outcome);
+        determineChoice();
     }
 
     private static final Map<String, String> shapesForSymbols;
@@ -19,9 +22,9 @@ public class Round {
         shapesForSymbols.put("B", "paper");
         shapesForSymbols.put("C", "scissors");
 
-        shapesForSymbols.put("X", "rock");
-        shapesForSymbols.put("Y", "paper");
-        shapesForSymbols.put("Z", "scissors");
+//        shapesForSymbols.put("X", "rock");
+//        shapesForSymbols.put("Y", "paper");
+//        shapesForSymbols.put("Z", "scissors");
     }
 
     private static final Map<String, Integer> scoresForShape;
@@ -35,22 +38,58 @@ public class Round {
     private static final Map<String, Integer> scoresForOutcome;
     static {
         scoresForOutcome = new HashMap<String, Integer>();
-        scoresForOutcome.put("lost", 0);
+        scoresForOutcome.put("lose", 0);
         scoresForOutcome.put("draw", 3);
         scoresForOutcome.put("win", 6);
     }
 
-    private void determineOutcome() {
-        if (opponentShape.equals(selectedShape)) {
-            outcome = "draw";
-        } else if (opponentShape.equals("rock") && selectedShape.equals("scissors")) {
-            outcome = "lost";
-        } else if (opponentShape.equals("scissors") && selectedShape.equals("paper")) {
-            outcome = "lost";
-        } else if (opponentShape.equals("paper") && selectedShape.equals("rock")) {
-            outcome = "lost";
-        } else {
-            outcome = "win";
+    private static final Map<String, String> outcomeForSymbols;
+    static {
+        outcomeForSymbols = new HashMap<String, String>();
+        outcomeForSymbols.put("X", "lose");
+        outcomeForSymbols.put("Y", "draw");
+        outcomeForSymbols.put("Z", "win");
+    }
+
+//    private void determineOutcome() {
+//        if (opponentShape.equals(selectedShape)) {
+//            outcome = "draw";
+//        } else if (opponentShape.equals("rock") && selectedShape.equals("scissors")) {
+//            outcome = "lose";
+//        } else if (opponentShape.equals("scissors") && selectedShape.equals("paper")) {
+//            outcome = "lose";
+//        } else if (opponentShape.equals("paper") && selectedShape.equals("rock")) {
+//            outcome = "lose";
+//        } else {
+//            outcome = "win";
+//        }
+//    }
+
+    private void determineChoice() {
+        if (opponentShape.equals("rock")) {
+            if (outcome.equals("lose")) {
+                selectedShape = "scissors";
+            } else if (outcome.equals("draw")) {
+                selectedShape = "rock";
+            } else if (outcome.equals("win")) {
+                selectedShape = "paper";
+            }
+        } else if (opponentShape.equals("paper")) {
+            if (outcome.equals("lose")) {
+                selectedShape = "rock";
+            } else if (outcome.equals("draw")) {
+                selectedShape = "paper";
+            } else if (outcome.equals("win")) {
+                selectedShape = "scissors";
+            }
+        } else if (opponentShape.equals("scissors")) {
+            if (outcome.equals("lose")) {
+                selectedShape = "paper";
+            } else if (outcome.equals("draw")) {
+                selectedShape = "scissors";
+            } else if (outcome.equals("win")) {
+                selectedShape = "rock";
+            }
         }
     }
 
