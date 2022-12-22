ac03585cbc741ad05b91f0aef373e4f9d4d8120d
AsgardianVoyager <86044128+AsgardianVoyager@users.noreply.github.com>
2022-12-22 19:22:20 +0100

    Day 8 **

--- Python/day8/day8.py
+++ Python/day8/day8.py
@@ -1,21 +1,51 @@
-from __future__ import annotations
 from dataclasses import dataclass
+import numpy as np
 
 
 @dataclass
-class File:
-    size: int
+class Tree:
+    i: int
+    j: int
+    height: int
 
+    def is_visible(self) -> bool:
 
-class Directory:
-    def __init__(self) -> None:
-        self.name: str = ""
-        self.files: list[File] = []
-        self.directories: list[Directory] = []
-        self.parent: None | Directory = None
+        return all(tree.height < self.height for tree in trees[self.i, :self.j]) or \
+               all(tree.height < self.height for tree in trees[self.i, self.j+1:]) or \
+               all(tree.height < self.height for tree in trees[:self.i, self.j]) or \
+               all(tree.height < self.height for tree in trees[self.i+1:, self.j])
 
-    def size(self):
-        return sum([file.size for file in self.files]) + sum([d.size() for d in self.directories])
+    def viewing_distance(self, direction: str) -> int:
+        distance = 0
+        match direction:
+            case "left":
+                for tree in reversed(trees[self.i, :self.j]):
+                    if tree.height <= self.height:
+                        distance += 1
+                    if tree.height >= self.height:
+                        break
+            case "right":
+                for tree in trees[self.i, self.j+1:]:
+                    if tree.height <= self.height:
+                        distance += 1
+                    if tree.height >= self.height:
+                        break
+            case "up":
+                for tree in reversed(trees[:self.i, self.j]):
+                    if tree.height <= self.height:
+                        distance += 1
+                    if tree.height >= self.height:
+                        break
+            case "down":
+                for tree in trees[self.i+1:, self.j]:
+                    if tree.height <= self.height:
+                        distance += 1
+                    if tree.height >= self.height:
+                        break
+        return distance
+
+    def scenic_score(self) -> int:
+        return self.viewing_distance("left")*self.viewing_distance("right")*self.viewing_distance("up")*self.viewing_distance("down")
 
 
 # Read and process input
@@ -24,10 +54,20 @@ with open(filename) as f:
     data = f.read().strip()
 lines = [line for line in data.split("\n")]
 
-
+trees = np.array([[Tree(i, j, int(height)) for j, height in enumerate(line)] for i, line in enumerate(lines)])
 
 # The 1st star
-
+visible = 0
+for row in trees:
+    for tree in row:
+        if tree.is_visible():
+            visible += 1
+print(visible)
 
 # The 2nd star
-
+highest_scenic_score = 0
+for row in trees:
+    for tree in row:
+        if tree.scenic_score() > highest_scenic_score:
+            highest_scenic_score = tree.scenic_score()
+print(highest_scenic_score)
